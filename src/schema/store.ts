1042bc8d426533b9b3a2d8a3b6c47a97334a61d0
seoeunbi25 <seoeunbi25@naver.com>
2025-07-28 03:08:50 +0900

    feat: 카카오맵 연결 & 가게 정보 반환 기능 작성

--- src/schema/store.ts
+++ src/schema/store.ts
@@ -7,7 +7,36 @@ const StoreSchema = new mongoose.Schema({
     address: { type: String, required: true },
     place_url: { type: String, required: true },
     phone: { type: String, default: "" },
-    x: { type: Number, required: true }, 
-    y: { type: Number, required: true },
+    latitude: { type: Number, required: true }, 
+    longitude: { type: Number, required: true },
     average_star: { type: Number, required: true },
 });
+
+const searchQuerySchema = Type.Object({
+    latitude: Type.Number(),
+    longitude: Type.Number()
+})
+
+const searchSchema = {
+    query: searchQuerySchema,
+    response: {
+        200: Type.Array(Object({
+            kakaoId: Type.String(),
+            name: Type.String(),
+            address: Type.String(),
+            place_url: Type.String(),
+            phone: Type.String(),
+            category_name: Type.String(),
+            latitude: Type.Number(),
+            longitude: Type.Number()
+        }))
+    }
+}
+
+export {
+    StoreSchema,
+
+    searchQuerySchema,
+
+    searchSchema
+}
--- src/schema/types/index.ts
+++ src/schema/types/index.ts
@@ -1,14 +1,17 @@
 import { Static } from '@sinclair/typebox'
 import { headers, registerBodySchema, loginBodySchema, resetPasswordBodySchema } from '../user'
+import { searchQuerySchema } from '../store'
 
 type THeaders = Static<typeof headers>
 type TUserRegisterBody = Static<typeof registerBodySchema>
 type TUserLoginBody = Static<typeof loginBodySchema>
 type TUserResetPasswordBody = Static<typeof resetPasswordBodySchema>
+type TStoreSearchQuery = Static<typeof searchQuerySchema>
 
 export {
     THeaders,
     TUserRegisterBody,
     TUserLoginBody,
     TUserResetPasswordBody,
+    TStoreSearchQuery,
 }
